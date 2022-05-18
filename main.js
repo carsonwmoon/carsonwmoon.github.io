e0fef2329dccf457a73a9b87aa16810f51a39238
moondude12 <moondude12@gmail.com>
2022-05-18 15:57:24 -0600

    Added my initial portfolio design

---
+++ main.js
@@ -0,0 +1,21 @@
+document.querySelectorAll('.toggle').forEach(toggle => {
+
+    toggle.querySelectorAll('.toggleContent').forEach(toggleContent => {
+        toggleContent.style.display = "none";
+    });
+
+    toggle.querySelectorAll('.triangle').forEach(triangle => {
+        triangle.addEventListener('click', (e) => {
+            toggle.querySelectorAll('.toggleContent').forEach(toggleContent => {
+                if (toggleContent.style.display === "block") {
+                    triangle.style.transform = "rotateZ(90deg)";
+                    toggleContent.style.display = "none";
+                } else {
+                    triangle.style.transform = "rotateZ(180deg)";
+                    toggleContent.style.display = "block";
+                }
+            });
+        })
+    });
+});
+
