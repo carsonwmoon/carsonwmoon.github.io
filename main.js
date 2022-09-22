9683b9342e52b4e8ef84bae5925c0d5aa1e36e21
moondude12 <moondude12@gmail.com>
2022-09-22 13:29:41 -0600

    Typo fix and triangle change.

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ document.querySelectorAll('.toggle').forEach(toggle => {
         triangle.addEventListener('click', (e) => {
             toggle.querySelectorAll('.toggleContent').forEach(toggleContent => {
                 if (toggleContent.style.display === "block") {
-                    triangle.style.transform = "rotateZ(90deg)";
+                    triangle.style.transform = "rotateZ(180deg)";
                     toggleContent.style.display = "none";
                 } else {
-                    triangle.style.transform = "rotateZ(180deg)";
+                    triangle.style.transform = "rotateZ(0deg)";
                     toggleContent.style.display = "block";
                 }
             });
